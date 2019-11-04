ad5c2ad60cea9785721a2741ec78cc7d41280840
fzabid05 <abidzora88@gmail.com>
2019-11-04 12:00:45 +0100

    add pargraph

--- index.php
+++ index.php
@@ -10,6 +10,8 @@
     <body>
         <div class="container">
             <h1 class="display-4 text-center text-info">Découvert de GITHUB</h1><hr>
+
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, incidunt, voluptatem suscipit amet quod velit dolores atque cupiditate quo qui doloribus beatae? Laudantium itaque doloribus ea omnis fugit dicta, praesentium obcaecati pariatur adipisci doloremque. Dolor, modi rerum? Quasi culpa repellendus aperiam unde asperiores deleniti qui officia a est labore illo voluptas ea voluptates, sint consequuntur tempore doloremque! Qui omnis quisquam nulla eius exercitationem voluptate ad est doloribus, voluptatem libero, repudiandae sapiente consectetur placeat veniam. Vero quis ullam, vitae magni iusto quam recusandae. Expedita aperiam obcaecati aspernatur, quam mollitia tenetur explicabo minima atque impedit perferendis nihil, veritatis beatae similique facilis ut!</p>
         </div>
     </body>
 </html>
